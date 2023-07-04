a9c9b6f87ca32b5e7271280fbaef3a28a8c26bee
Samuel Cobb <sjcobb2003@gmail.com>
2023-07-04 21:31:23 +0100

    testing config

--- dap/adapters.lua
+++ dap/adapters.lua
@@ -1,32 +1,35 @@
-return {
-  -- haskell-debug-adapter setup
-  dap = {
-    adapters = {
-      -- haskell = {
-      --   type = "executable",
-      --   command = vim.fn.exepath "haskell-debug-adapter",
-      -- },
-    },
-    configurations = {
-      --   haskell = {
-      --     {
-      --       type = "haskell",
-      --       request = "launch",
-      --       name = "Debug",
-      --       workspace = "${workspaceFolder}",
-      --       startup = "${file}",
-      --       stopOnEntry = true,
-      --       logFile = vim.fn.stdpath "data" .. "/haskell-dap.log",
-      --       logLevel = "WARNING",
-      --       ghciEnv = vim.empty_dict(),
-      --       ghciPrompt = ">",
-      --       -- Adjust the prompt to the prompt you see when you invoke the stack ghci command below
-      --       ghciInitialPrompt = ">",
-      --       ghciCmd = "stack ghci --with-ghc="
-      --         .. vim.fn.exepath "ghci-dap"
-      --         .. " --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show --ghci-options -ignore-dot-ghci",
-      --     },
-      --   },
-    },
-  },
-}
+return function()
+  if(vim.fn.isdirectory('/etc/nixos')) then
+    return {
+      dap = {
+        adapters = {
+          haskell = {
+            type = "executable",
+            command = vim.fn.exepath "haskell-debug-adapter",
+          },
+        },
+        configurations = {
+          haskell = {
+            {
+              type = "haskell",
+              request = "launch",
+              name = "Debug",
+              workspace = "${workspaceFolder}",
+              startup = "${file}",
+              stopOnEntry = true,
+              logFile = vim.fn.stdpath "data" .. "/haskell-dap.log",
+              logLevel = "WARNING",
+              ghciEnv = vim.empty_dict(),
+              ghciPrompt = ">",
+              -- Adjust the prompt to the prompt you see when you invoke the stack ghci command below
+              ghciInitialPrompt = ">",
+              ghciCmd = "stack ghci --with-ghc="
+                .. vim.fn.exepath "ghci-dap"
+                .. " --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show --ghci-options -ignore-dot-ghci",
+            },
+          },
+        },
+      },
+    }
+  end
+end
--- lsp/skip_setup.lua
+++ lsp/skip_setup.lua
@@ -2,4 +2,5 @@ return {
   -- had issues with PHP
   -- "phpactor"
   -- "intelephense"
+  "haskell-language-server"
 }
